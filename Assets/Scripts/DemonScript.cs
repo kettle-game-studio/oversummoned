57a2f8f20e425bd77d3197918a79d2d45fa4c6da
Tetrergeru <ArutunoffOleg@yandex.ru>
2024-04-14 12:35:18 +0200

    Add demon components shuffle

--- Assets/Scripts/DemonScript.cs
+++ Assets/Scripts/DemonScript.cs
@@ -7,6 +7,33 @@ public class DemonScript : MonoBehaviour
     public string Name;
     public Rigidbody DemonBody;
 
+    public SpriteRenderer[] Bodies;
+    public SpriteRenderer[] Heads;
+    public SpriteRenderer[] Faces;
+    public SpriteRenderer[] Horns;
+    public SpriteRenderer[] Features;
+
+    private SpriteRenderer[][] partsArrays;
+
+    void Start()
+    {
+        partsArrays = new SpriteRenderer[][]{ Bodies, Heads, Faces, Horns, Features };
+        Shuffle();
+    }
+
+    void Shuffle()
+    {
+        foreach (var parts in partsArrays)
+        {
+            foreach(var part in parts)
+            {
+                part.enabled = false;
+            }
+            var idx = Random.Range(0, parts.Length);
+            parts[idx].enabled = true;
+        }
+    }
+
     public void StartBeHeld()
     {
         DemonBody.isKinematic = true;
