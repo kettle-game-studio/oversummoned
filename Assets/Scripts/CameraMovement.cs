34aebfe03abdd81829a953ac9b3734caa92c24cd
RobolabGs2 <slavikelk@yandex.ru>
2024-04-15 20:28:39 +0300

    Fix demon dragging, add moving by mouse

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -10,6 +10,8 @@ public class CameraMovement : MonoBehaviour
     public float RotationSpeed = 10;
     public float DesiredGrabbedDemonHeights = 3f;
 
+    public float CameraMouseMoveThreshold = 0.05f;
+
     public GameObject LassoPrefab;
 
     public Camera Camera;
@@ -17,29 +19,29 @@ public class CameraMovement : MonoBehaviour
     private DemonScript Demon;
     private LineRenderer lasso;
 
-    void FixedUpdate()
+    void Update()
     {
         transform.Translate(
-            new Vector3(
-                Input.GetAxis("Horizontal") * Time.deltaTime * CameraSpeed,
-                0,
-                Input.GetAxis("Vertical") * Time.deltaTime * CameraSpeed
-            ),
+            CameraMoveVector() * Time.deltaTime * CameraSpeed,
             Space.Self
         );
 
-        if (Input.GetMouseButton(1)) {
+        if (Input.GetMouseButton(1))
+        {
             transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * RotationSpeed, Space.World);
         }
 
-        if (Input.GetMouseButtonDown(0) && Demon == null) {
+        if (Input.GetMouseButtonDown(0) && Demon == null)
+        {
             var p0 = MouseRayStart();
 
             // Debug.Log($"LayerMask.NameToLayer = {LayerMask.NameToLayer("Demon")}");
             var didHit = Physics.Raycast(p0, Camera.transform.forward * 10, out var hitInfo, 1000, ~(1 << LayerMask.NameToLayer("Wall")));
-            if (didHit) {
-                var demon =  hitInfo.collider.GetComponent<DemonScript>();
-                if (demon != null) {
+            if (didHit)
+            {
+                var demon = hitInfo.collider.GetComponent<DemonScript>();
+                if (demon != null)
+                {
                     // Debug.Log($"Demon {demon.Name}");
                     Demon = demon;
                     Demon.StartBeHeld();
@@ -48,7 +50,8 @@ public class CameraMovement : MonoBehaviour
             }
         }
 
-        if (!Input.GetMouseButton(0) && Demon != null) {
+        if (!Input.GetMouseButton(0) && Demon != null)
+        {
             Demon.StopBeHeld();
             Demon = null;
             Destroy(lasso.gameObject);
@@ -58,7 +61,8 @@ public class CameraMovement : MonoBehaviour
         SetDemonPosition();
     }
 
-    void SetDemonPosition() {
+    void SetDemonPosition()
+    {
         if (Demon == null) return;
 
         var p0 = MouseRayStart();
@@ -70,16 +74,38 @@ public class CameraMovement : MonoBehaviour
         var dist = Mathf.Min((desiredPosition - Demon.DemonBody.transform.position).magnitude, 1);
         Demon.DemonBody.velocity = (desiredPosition - Demon.DemonBody.transform.position).normalized * dist * 10;
 
-        for (var i = 0; i < lasso.positionCount; i++) {
+        for (var i = 0; i < lasso.positionCount; i++)
+        {
             lasso.SetPosition(i, Vector3.Lerp(desiredPosition, Demon.DemonBody.transform.position, (float)i / (lasso.positionCount - 1)));
         }
     }
 
     Vector3 MouseRayStart()
     {
-        var clickXCoeff =  (2 * (Input.mousePosition.x / Camera.pixelWidth) - 1) * Camera.orthographicSize * Camera.aspect; 
-        var clickYCoeff =  (2 * (Input.mousePosition.y / Camera.pixelHeight) - 1) * Camera.orthographicSize; 
+        var clickXCoeff = (2 * (Input.mousePosition.x / Camera.pixelWidth) - 1) * Camera.orthographicSize * Camera.aspect;
+        var clickYCoeff = (2 * (Input.mousePosition.y / Camera.pixelHeight) - 1) * Camera.orthographicSize;
         var rayOffset = Camera.transform.right * clickXCoeff + Camera.transform.up * clickYCoeff;
         return Camera.transform.position + rayOffset;
+    }
+
+    float ScalarMouseDirection(float mousePos, float winSize)
+    {
+        float threshold = CameraMouseMoveThreshold;
+        if (mousePos < 0 || mousePos > winSize) return 0;
+        var thresholdPx = winSize * threshold;
+        if (mousePos < thresholdPx)
+            return -1;
+        if (winSize - mousePos < thresholdPx)
+            return 1;
+        return 0;
+    }
+    Vector3 CameraMoveVector()
+    {
+        var mousePos = Input.mousePosition;
+        return new Vector3(
+                Input.GetAxis("Horizontal") + ScalarMouseDirection(mousePos.x, Screen.width),
+                0,
+                Input.GetAxis("Vertical") + ScalarMouseDirection(mousePos.y, Screen.height)
+            );
     }
 }
